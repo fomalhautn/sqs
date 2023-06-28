64c80e4789333b6402df29cd644fb8a9665db7c1
fomalhautn <79667606+fomalhautn@users.noreply.github.com>
2023-06-28 22:47:53 +0200

    author

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -11,12 +11,11 @@ type Props = {
  // author: Author
 }
 
-const PostHeader = ({ title, coverImage, date, author }: Props) => {
+const PostHeader = ({ title, coverImage, date }: Props) => {
   return (
     <>
       <PostTitle>{title}</PostTitle>
       <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
@@ -34,4 +33,5 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
 }
 
 export default PostHeader
+//         <Avatar name={author.name} picture={author.picture} />
 //           <Avatar name={author.name} picture={author.picture} />
