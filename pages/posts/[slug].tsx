811103f9ba33370a63705f3dff22f08eaf6ed4a1
fomalhautn <79667606+fomalhautn@users.noreply.github.com>
2023-06-28 18:28:36 +0200

    3 files modified

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -20,7 +20,7 @@ type Props = {
 
 export default function Post({ post, morePosts, preview }: Props) {
   const router = useRouter()
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`
+  const title = `${post.title} | Swedish Quantum Society`
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
   }
